d0f5451e5d9cd94e823ffc8114596adb0e1c6d24
Chokcha Homsombat <48384299+chokchai9900@users.noreply.github.com>
2021-01-14 16:33:47 +0700

    commit first

---
+++ DependencyServiceDemos/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using ZXing;
+
+namespace DependencyServiceDemos
+{
+    public partial class MainPage : TabbedPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        void OnGetDeviceOrientationButtonClicked(object sender, EventArgs e)
+        {
+            DeviceOrientation orientation = DependencyService.Get<IDeviceOrientationService>().GetOrientation();
+            orientationLabel.Text = orientation.ToString();
+        }
+
+        async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+        {
+            var bcreader = new BarcodeReader();
+
+            (sender as Button).IsEnabled = false;
+
+            Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+            if (stream != null)
+            {
+                img.Source = ImageSource.FromStream(() => stream);
+            }
+
+            var getclass = new decodeBarcode();
+
+            var result = getclass.Barcodedecoder(stream);
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned Barcode", $"Data is {result}", "OK");
+            });
+
+            (sender as Button).IsEnabled = true;
+
+        }
+
+
+        async void OnSpeakButtonClicked(object sender, EventArgs e)
+        {
+            ITextToSpeechService service = DependencyService.Get<ITextToSpeechService>(DependencyFetchTarget.NewInstance);
+            using (service as IDisposable)
+            {
+                await service.SpeakAsync("Hello world");
+            }
+        }
+
+    }
+}
---
+++ DependencyServiceDemos/decodeBarcode.cs
@@ -0,0 +1,35 @@
+﻿using Android.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using ZXing;
+
+namespace DependencyServiceDemos
+{
+    public class decodeBarcode
+    {
+        public string Barcodedecoder(Stream stream)
+        {
+            var input = BitmapFactory.DecodeStream(stream);
+            var imgArray = GetRgbBytes(input);
+
+            var bcreader = new BarcodeReader();
+            var result = bcreader.Decode(imgArray,input.Width,input.Height,RGBLuminanceSource.BitmapFormat.Unknown);
+
+            return result.Text;
+        }
+
+        private static byte[] GetRgbBytes(Bitmap image)
+        {
+            var rgbBytes = new List<byte>();
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var c = new Android.Graphics.Color(image.GetPixel(x, y));
+                    rgbBytes.AddRange(new[] { c.R, c.G, c.B });
+                }
+            }
+            return rgbBytes.ToArray();
+        }
+    }
+}
