9167671527abaec221f71b094ba894198dca3fb2
taninpong <taninpong.ch.56@ubu.ac.th>
2021-01-15 14:41:40 +0700

    S3

--- DependencyServiceDemos/decodeBarcode.cs
+++ DependencyServiceDemos/decodeBarcode.cs
@@ -12,7 +12,6 @@ namespace DependencyServiceDemos
     {
         public async Task<string> Barcodedecoder(Stream stream)
         {
-
             var image = await CrossImageEdit.Current.CreateImageAsync(stream);
             var bytes = image.ToArgbPixels()
                 .Select(it =>
@@ -20,9 +19,12 @@ namespace DependencyServiceDemos
                     var color = System.Drawing.Color.FromArgb(it);
                     return new[] { color.R, color.G, color.B };
                 }).SelectMany(it => it).ToArray();
-
+            if (image.Width < 1281 && image.Height < 961)
+            {
+                image = image.Resize(1280, 960);
+            }
             var bcreader = new BarcodeReader();
-            var result = bcreader.Decode(bytes, image.Width, image.Height, RGBLuminanceSource.BitmapFormat.Unknown);
+            var result = bcreader.Decode(bytes, image.Width, image.Height, RGBLuminanceSource.BitmapFormat.RGB24);
             if (result == null)
             {
                 return "Nosupport";
@@ -33,45 +35,45 @@ namespace DependencyServiceDemos
             }
         }
 
-        public string Getcode(string result)
-        {
-            return result;
-        }
+        //public string Getcode(string result)
+        //{
+        //    return result;
+        //}
 
-        private static byte[] GetRgbBytesX(int[] pixel)
-        {
-            var rgbBytes = new List<byte>();
-            foreach (var item in pixel)
-            {
-                var r = item & 0x00FF0000 >> 16; //Get R
-                var g = item & 0x0000FF00 >> 8;  //Get G
-                var b = item & 0x000000FF;       //Get B
+        //private static byte[] GetRgbBytesX(int[] pixel)
+        //{
+        //    var rgbBytes = new List<byte>();
+        //    foreach (var item in pixel)
+        //    {
+        //        var r = item & 0x00FF0000 >> 16; //Get R
+        //        var g = item & 0x0000FF00 >> 8;  //Get G
+        //        var b = item & 0x000000FF;       //Get B
 
-                byte bytesR = Convert.ToByte(r.ToString());
-                byte bytesG = Convert.ToByte(g.ToString());
-                byte bytesB = Convert.ToByte(b.ToString());
+        //        byte bytesR = Convert.ToByte(r.ToString());
+        //        byte bytesG = Convert.ToByte(g.ToString());
+        //        byte bytesB = Convert.ToByte(b.ToString());
 
-                rgbBytes.AddRange(new[] { bytesR, bytesG, bytesB });
-            }
-            return rgbBytes.ToArray();
-        }
+        //        rgbBytes.AddRange(new[] { bytesR, bytesG, bytesB });
+        //    }
+        //    return rgbBytes.ToArray();
+        //}
 
-        public async Task<string> xDecode(Stream stream)
-        {
-            var image = await CrossImageEdit.Current.CreateImageAsync(stream);
-            var pixels = image.ToArgbPixels();
-            var imgArray = GetRgbBytesX(pixels);
-            var bcreader = new BarcodeReader();
-            var result = bcreader.Decode(imgArray, image.Width, image.Height, RGBLuminanceSource.BitmapFormat.Unknown);
-            if (result == null)
-            {
-                return "Nosupport";
-            }
-            else
-            {
-                return result.Text;
-            }
+        //public async Task<string> xDecode(Stream stream)
+        //{
+        //    var image = await CrossImageEdit.Current.CreateImageAsync(stream);
+        //    var pixels = image.ToArgbPixels();
+        //    var imgArray = GetRgbBytesX(pixels);
+        //    var bcreader = new BarcodeReader();
+        //    var result = bcreader.Decode(imgArray, image.Width, image.Height, RGBLuminanceSource.BitmapFormat.Unknown);
+        //    if (result == null)
+        //    {
+        //        return "Nosupport";
+        //    }
+        //    else
+        //    {
+        //        return result.Text;
+        //    }
 
-        }
+        //}
     }
 }
